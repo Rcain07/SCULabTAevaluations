6b562ee3464ffb061b2c2624c281cff2749a2ea5
Ryan Cain <sideways.008@gmail.com>
2019-11-14 15:30:54 -0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    # ...
+    if os.environ.get('DATABASE_URL'):
+        'postgres+psycopg2://sculabtadatabase:coen174!@database.azure.com:5432/sculabta'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
