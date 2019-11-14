4f2f07e7bbacabd12c89c2116deb8902a4682a8b
Ryan Cain <sideways.008@gmail.com>
2019-11-14 15:39:17 -0800

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     if os.environ.get('DATABASE_URL'):
-        'postgres+psycopg2://sculabtadatabase:coen174!@database.azure.com:5432/sculabta'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        db ='postgres+psycopg2://sculabtadatabase:coen174!@database.azure.com:5432/sculabta'
+    SQLALCHEMY_DATABASE_URI = db or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
